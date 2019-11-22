44cdea7e181a4a695072c11387aeb78f2daa0850
Anton Punko <ant.punko@gmail.com>
2019-11-22 17:16:27 +0300

    set up base api models

---
+++ db/migrate/20191122140536_create_contents.rb
@@ -0,0 +1,12 @@
+class CreateContents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contents do |t|
+      t.string :title
+      t.text :plot
+      t.integer :number
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
