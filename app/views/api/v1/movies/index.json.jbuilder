b10607badd55fb511059bcbb6a13807e1fd97d08
Anton Punko <ant.punko@gmail.com>
2019-11-27 00:00:41 +0300

    Define movie attributes explicitly

--- app/views/api/v1/movies/index.json.jbuilder
+++ app/views/api/v1/movies/index.json.jbuilder
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
-json.data @movies
+json.data do
+  json.array! @movies do |movie|
+    json.id movie.id
+    json.title movie.title
+    json.plot movie.plot
+    json.number movie.number
+    json.created_at movie.created_at
+  end
+end
--- spec/requests/api/v1/movies_spec.rb
+++ spec/requests/api/v1/movies_spec.rb
@@ -4,62 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::MoviesController, type: :request do
   describe '#index' do
-    it ':success on default' do
+    it_behaves_like 'paginated_endpoint', Movie
+
+    it 'a movie contains id, title, plot, number, created_at attributes' do
+      create(:movie)
       get api_v1_movies_path
-      expect(response).to have_http_status(:success)
-    end
 
-    it ':success on explicit json format' do
-      get api_v1_movies_path(format: :json)
-      expect(response).to have_http_status(:success)
-    end
-
-    it ':not_acceptable on not jso format' do
-      get api_v1_movies_path(format: :html)
-      expect(response).to have_http_status(:not_acceptable)
-    end
-
-    it 'handles page param' do
-      get api_v1_movies_path, params: { page: Faker::Number.digit }
-      expect(response).to have_http_status(:success)
-    end
-
-    describe 'paginator' do
-      let!(:movies) { create_list(:movie, Movie.default_per_page + 1) }
-
-      it 'returns content per page' do
-        get api_v1_movies_path, params: { page: 1 }
-        expect(JSON.parse(response.body).dig('data').count).to eq Movie.default_per_page
-      end
-
-      it 'could returns reduced page size for last page' do
-        get api_v1_movies_path, params: { page: 2 }
-        expect(JSON.parse(response.body).dig('data').count).to eq 1
-      end
-
-      it 'returns empty result set for page after last' do
-        get api_v1_movies_path, params: { page: 3 }
-        expect(JSON.parse(response.body).dig('data').count).to eq 0
-      end
-    end
-
-    describe 'default order' do
-      let!(:movies) { create_list(:movie, Movie.default_per_page) }
-
-      it 'returns ordered by creation result set' do
-        get api_v1_movies_path, params: { page: 1 }
-
-        movies = JSON.parse(response.body).dig('data')
-        expect(movies.first['created_at']).to be > movies.last['created_at']
-      end
-
-      it 'the first on the first page is just added movie' do
-        movie = create(:movie)
-        get api_v1_movies_path, params: { page: 1 }
-
-        movies = JSON.parse(response.body).dig('data')
-        expect(movies.first['id']).to be movie.id
-      end
+      movie = JSON.parse(response.body).dig('data').first
+      expect(movie.keys.sort).to eq %w[id title plot number created_at].sort
     end
   end
 end
